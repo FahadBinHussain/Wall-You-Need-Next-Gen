0a3072d9a8728886283831e9c4cb2edd0e2206c9
Fahad <fahadbinhussain001@gmail.com>
2025-04-09 19:46:13 +0600

    add placeholder and grid inside latestwallpaperpage

--- Views/LatestWallpapersPage.xaml.cs
+++ Views/LatestWallpapersPage.xaml.cs
@@ -1,19 +1,44 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using Wall_You_Need_Next_Gen.Models;
+using Wall_You_Need_Next_Gen.Services;
 
 namespace Wall_You_Need_Next_Gen.Views
 {
     public sealed partial class LatestWallpapersPage : Page
     {
+        private WallpaperService _wallpaperService;
+
         public LatestWallpapersPage()
         {
             this.InitializeComponent();
+            _wallpaperService = new WallpaperService();
+            LoadWallpapers();
         }
 
-        private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        private void LoadWallpapers()
         {
-            // Simple click handler
-            ClickMeButton.Content = "Button Clicked!";
+            // Get the latest wallpapers from the service
+            var wallpapers = _wallpaperService.GetLatestWallpapers(12);
+
+            // Add wallpaper cards to the grid
+            for (int i = 0; i < wallpapers.Count; i++)
+            {
+                var row = i / 3;
+                var column = i % 3;
+                
+                var card = new WallpaperCard();
+                card.SetWallpaper(wallpapers[i]);
+                card.Margin = new Thickness(8);
+                card.Height = 180;
+                
+                Grid.SetRow(card, row);
+                Grid.SetColumn(card, column);
+                
+                WallpapersGrid.Children.Add(card);
+            }
         }
     }
 } 
